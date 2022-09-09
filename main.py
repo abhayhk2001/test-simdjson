8dcf51ca415a357fe3280d3a03ad483d17595db2
Abhay H Kashyap <abhayastro1004@gmail.com>
2022-09-09 14:17:54 +0530

    Transformations complete and working

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 import simdjson
 import pandas as pd
+from datetime import datetime as dt
 
 parser = simdjson.Parser()
 doc = parser.load("data/sample_data2.json", True)
-
 df = pd.DataFrame(doc)
 
-
-df['timestamp'] = pd.to_datetime(df['timestamp'])
-# df['timestamp'] = df['timestamp'].astype('datetime64[ns]')
-df['timestamp'] = df['timestamp'].astype('datetime64[us]')
-
+df['timestamp'] = df['timestamp'].apply(lambda x: dt.fromtimestamp(x/1000))
 df = df.pivot_table(index=['timestamp', 'device_uuid'],
                     columns='data_item_name', aggfunc='first')
-# df.columns.set_levels([None], level=0, inplace=True)
 df.columns.set_levels([""], level=0, inplace=True)
 
-
-df.to_parquet("data/sample_data2.parquet", engine='pyarrow')
-# change Columns names by reading from parquet and then writing back
+df.to_parquet("data/sample_data2.parquet", engine="pyarrow")
 print(df)
