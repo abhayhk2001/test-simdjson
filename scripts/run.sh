36b367de4c9d210c62682784d05aa2363904be0c
Abhay H Kashyap <abhayastro1004@gmail.com>
2022-09-25 15:09:34 +0530

    Fixed scripts for the running collectd

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,31 +1,27 @@
-### remove all results 
-rm output/*
-
-### download output files
-readlink -f output/
-
-# Configuring Collectd\
+# Configuring Collectd
 sudo vim /etc/collectd/collectd.conf
 
-
 ## Running Collectd
+cd test-simdjson/
+rm output/* extras/*
+source env/bin/activate
+sudo rm -rf /var/lib/collectd/csv/
 sudo systemctl stop collectd.service
 sudo systemctl start collectd.service
-cd test-simdjson/
-source env/bin/activate
-echo $EPOCHREALTIME
 python collectd/main.py
 sudo systemctl stop collectd.service
-
-## find output file
-readlink -f /var/lib/collectd/csv/intel-ather-test.c.lucky-rookery-360910.internal/memory/memory-u<tab>
 
 ## Move Output CSV file
 cp /var/lib/collectd/csv/intel-ather-test.c.lucky-rookery-360910.internal/memory/memory-u<tab> ~/test-simdjson/extras/collectd_memory.csv
 
 ## Consolidate collectd files
-
-
+python collectd/parse_results.py
+zip -r output.zip output
+clear
+readlink -f output.zip
 
 ## remove all files in csv
 sudo rm -rf /var/lib/collectd/csv/
+
+## Remove output.zip file
+rm output.zip
