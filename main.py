e7834fb99e7ad49ba1eae0b536b9efbb532700a6
Abhay H Kashyap <abhayastro1004@gmail.com>
2022-10-21 16:31:03 +0530

    Refactor Tracemalloc branch

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from time import time
 import gzip
 import os
+import tracemalloc
 
 
 def format_file(filename, count):
@@ -23,135 +24,98 @@ def format_file(filename, count):
     fw.close()
     return f"./data/{filename[:-5] + '_modified.json'}"
 
-
-def calc_increase(prev, time, row):
-    if (len(prev) == 0):
-        increase = ''
-    else:
-        increase = ((time - prev[len(row)])/prev[len(row)]) * 100
-        increase = str(round(increase, 3)) + '%'
-
-    return ([round(time, 4), increase])
-
-
-def compare_gz(parquet_gz, filename):
-    with open(parquet_gz, 'rb') as f_in, gzip.open(parquet_gz+'.gz', 'wb') as f_out:
-        f_out.writelines(f_in)
-    json_gz_size = os.path.getsize(f'./data/{filename}.gz')
-    parquet_gz_size = os.path.getsize(parquet_gz+'.gz')
-    print("Json .gz file size: ", json_gz_size, " bytes")
-    print("Parquet .gz file size: ", parquet_gz_size, " bytes")
-    os.remove(parquet_gz+'.gz')
-
-
-def create_df(rows, counts):
-    col_name = ['Formatting', 'Loading File',
-                'Pivotting Table', 'Converting Table to Parquet']
-    col_name_sub = ['Time', 'Increase']
-    cols = [('File',)]
-    for i in col_name:
-        for j in col_name_sub:
-            cols.append((i, j))
-    cols.extend([('Total', 'Time'), ('Total', 'Increase')])
-    col_list = pd.MultiIndex.from_tuples(cols)
-    df = pd.DataFrame(rows, counts, col_list)
-    df.to_excel(f"./output/results.xlsx")
-
-    print(df)
-
-
-def make_time_df(rows, counts):
-    cols = ['Start', 'Formatting', 'Loading File',
-            'Pivotting Table', 'Converting Table to Parquet']
-    df = pd.DataFrame(rows, counts, cols)
-    df.to_csv(f"./output/results_time.csv")
+def new_algo(df):
+    print(len(df['data_item_name'].unique()))
+    pd.DataFrame()
     pass
 
-
-def run(findex, file, count, prev):
-    # Starting Time Recording
-    total_time = 0
-    row = [findex]
-    time_row = []
+def run(count):
     start = time()
-    time_row.append(start)
-
-    # Formatting JSON
-    filename = format_file(file, count)
+    filename = format_file("sample_json_test_data_2.json", count)
     format_time = time() - start
-    total_time += format_time
-    time_row.append(start + format_time)
-    row.extend(calc_increase(prev, format_time, row))
+    if(len(prev) == 0):
+        format_increase = ''
+    else:
+        format_increase = ((format_time - prev[len(row)])/prev[len(row)]) * 100
+        format_increase = str(round(format_increase,3)) + '%'
 
-    start = time()
-    # Loading JSON file to Python Dataframe (simdjson vs polars)
+    current, peak = tracemalloc.get_traced_memory()
+    row.extend([round(format_time,4),format_increase, current / 10**6, peak / 10**6])
+
     parser = simdjson.Parser()
     doc = parser.load(filename, True)
     df = pd.DataFrame(doc)
-    load_time = time() - start
-    total_time += load_time
-    time_row.append(start + load_time)
-    row.extend(calc_increase(prev, load_time, row))
+    load_time = time() - format_time - start
+    if(len(prev) == 0):
+        load_increase = ''
+    else:
+        load_increase = ((load_time - prev[len(row)])/prev[len(row)]) * 100
+        load_increase = str(round(load_increase,3)) + '%'
 
-    start = time()
-    # Using Pivot table to recieve appropriate output
+    current, peak = tracemalloc.get_traced_memory()
+    row.extend([round(load_time,4),load_increase, current / 10**6, peak / 10**6])
+
+    #new_algo(df)
     df = df.pivot_table(index=['timestamp', 'device_uuid'],
                         columns='data_item_name', values='value', aggfunc='first')
-    pivot_time = time() - start
-    total_time += pivot_time
-    time_row.append(start + pivot_time)
-    row.extend(calc_increase(prev, pivot_time, row))
+    pivot_time = time() - load_time - start
+    if(len(prev) == 0):
+        pivot_increase = ''
+    else:
+        pivot_increase = ((pivot_time - prev[len(row)])/prev[len(row)]) * 100
+        pivot_increase = str(round(pivot_increase,3)) + '%'
+    current, peak = tracemalloc.get_traced_memory()
+    row.extend([round(pivot_time,4),pivot_increase, current / 10**6, peak / 10**6])
 
-    start = time()
-    # Converting to Parquet
     df.to_parquet(f"{filename[:-5]}.parquet", engine="pyarrow")
-    convert_time = time() - start
-    total_time += convert_time
-    time_row.append(start + convert_time)
-    row.extend(calc_increase(prev, convert_time, row))
-    return (filename, f"{filename[:-5]}.parquet", total_time, row, time_row)
+    convert_time = time() - pivot_time - start
+    if(len(prev) == 0):
+        convert_increase = ''
+    else:
+        convert_increase = ((convert_time - prev[len(row)])/prev[len(row)]) * 100
+        convert_increase = str(round(convert_increase,3)) + '%'
+    
+    current, peak = tracemalloc.get_traced_memory()
+    row.extend([round(convert_time,4),convert_increase, current / 10**6, peak / 10**6])
+
+    total_time = time() - start
+    if(len(prev) == 0):
+        total_increase = ''
+    else:
+        total_increase = ((total_time - prev[len(row)])/prev[len(row)]) * 100
+        total_increase = str(round(total_increase,3)) + '%'
+
+    row.extend([round(total_time,4),total_increase])
+    return (filename, f"{filename[:-5]}.parquet")
 
 
-def main(file_counts):
-    rows, prev, time_rows = [], [], []
-    final_parquet = ""
-    counts = []
-    for i, (findex, file, count) in enumerate(file_counts):
-        counts.append(count)
-        if count == 0:
-            print(f"Running for whole file")
-        else:
-            print(f"Running for count: {count}")
+counts = [500, 1000, 5000, 10000, 50000, 100000, 124703]
 
-        (fname, parquet_name, total_time, row,
-         time_row) = run(findex, file, count, prev)
+rows = []
+prev = []
+for count in counts:
+    row = []
+    print(f"Running for count: {count}")
+    tracemalloc.start()
+    (fname, parquet_name) = run(count)
+    tracemalloc.stop()
+    prev = row
+    print("\n\n")
+    os.remove(fname)
+    # os.remove(parquet_name)
+    rows.append(row)
 
-        row.extend(calc_increase(prev, total_time, row))
-        prev = row
-        os.remove(fname)
-        if (i != len(file_counts)-1):
-            os.remove(parquet_name)
-        else:
-            final_parquet = "./extras" + parquet_name[6:]
-            os.replace(parquet_name, "./extras"+parquet_name[6:])
-        rows.append(row)
-        time_rows.append(time_row)
-    compare_gz(final_parquet, file_counts[-1][1])
-    create_df(rows, counts)
-    make_time_df(time_rows, counts)
+col_name = ['Formatting', 'Loading File',
+            'Pivotting Table', 'Converting Table to Parquet']
+col_name_sub = ['Time', 'Increase', 'Peak(MB)', 'Current(MB)']
+cols = []
+for i in col_name:
+    for j in col_name_sub:
+        cols.append((i, j))
+cols.extend([('Total','Time'),('Total','Increase')])
+col_list = pd.MultiIndex.from_tuples(cols)
 
+df = pd.DataFrame(rows, counts, col_list)
+print(df)
 
-fname1 = "connectdata-day=2022-09-19_device=s_96_0.json"
-fname2 = "connectdata-day=2022-09-19_device=s_96_2.json"
-fname3 = "connectdata-day=2022-09-19_device=s_96_3.json"
-fname4 = "connectdata-day=2022-09-19_device=s_96_4.json"
-
-# main([("file1","sample_json_test_data_2.json", 124037)])
-# main([("file1",fname1, 453132)])
-# main([("file1",fname2, 836753)])
-# main([("file1",fname3, 1000000)])
-# main([("file3", fname3, 1000000), ("file2", fname2, 836753), ("file1", fname1, 453132),("file3", fname3, 836753), ("file2", fname2, 453132), ("file3", fname3, 453132)])
-# main([("file2", fname2, 453132), ("file2", fname2, 500000), ("file2", fname2, 550000), ("file2", fname2, 600000), ("file2", fname2, 700000), ("file2", fname2, 800000), ("file2", fname2, 836753)])
-# main([("file3", fname3, 100000), ("file3", fname3, 200000), ("file3", fname3, 300000), ("file3", fname3, 400000), ("file3", fname3, 500000), ("file3",fname3, 600000), ("file3", fname3, 700000), ("file3", fname3, 800000), ("file3", fname3, 836753), ("file3", fname3, 900000), ("file3", fname3, 1000000)])
-# main([("file3", fname3, 836753), ("file3", fname3, 836754)])
-# main([('file4', fname2, 5000000)])
+df.to_excel("./output/results.xlsx")
